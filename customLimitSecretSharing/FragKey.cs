6c01e97103501b5b5f6a1ebc9d56a6bf660cfa6b
klidDjanko <service.1.storage@gmail.com>
2021-05-10 21:10:07 +0300

    Разделение секрета с произвольным порогом

---
+++ customLimitSecretSharing/FragKey.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace customLimitSecretSharing
+{
+    class FragKey
+    {
+        //Здесь описан метод разделения вектор ключа на части секрета
+        //для последующего разделения их между пользователями мажоритарным способом
+        //Метод получает количество фрагментов, на которое надо разбить вектор ключ, ссылку на сам вектор клsegmentsюч, простое число simpleNum
+        public int[,] Frag(int segments, int simpleNum, ref List<int> key)
+        {
+            int[,] frag = new int[segments, key.Count];
+            //Первые segmentCount - 1 сегментов мы генерируем случайно
+            Random random = new Random(DateTime.UtcNow.Millisecond);
+            for (int i = 0; i < segments - 1; i++)
+            {
+                for (int j = 0; j < key.Count; j++)
+                {
+                    frag[i, j] = random.Next(0, simpleNum);
+                }
+            }
+            //Последние сегменты генерируются как разность от соответв. элемента вектор ключа
+            //со всеми соотв. сгенерированными элементами и взятая по модулю simpleNUm
+            //Внешний цикл идёт по столбцам матрицы, в которой храняться фрагменты секрета
+            for (int k = 0; k < key.Count; k++)
+            {
+                int keyElem = key[k];
+                //Внутренний цикл идёт по строкам матрицы, в которой храняться фрагменты секрета
+                for (int s = 0; s < segments - 1; s++)
+                {
+                    keyElem -= frag[s, k];
+                }
+
+                //Выполняем mod simpleNum
+                if (keyElem < 0) keyElem = (keyElem % simpleNum) + simpleNum;
+                else keyElem = keyElem % simpleNum;
+
+                frag[segments - 1, k] = keyElem;
+            }
+
+            return frag;
+        }
+    }
+}
---
+++ customLimitSecretSharing/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace customLimitSecretSharing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число сотрудников N (целое и > 1): ");
+            //Число сотрудников, оно должно быть больше единицы
+            int n = 1;
+            while (n == 1 || n < 1)
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+                if (n == 1 || n < 1) Console.WriteLine("N должно быть больше 1, повторите ввод: ");
+            }
+
+            //Пороговое число (минимальное число сотрудников, которые могут восстановить ключ)
+            Console.Write("Введите пороговое число h: ");
+            int h = Convert.ToInt32(Console.ReadLine());
+
+            //Количество сегментов, на которые разбивается ключ вектор
+            Program program = new Program();
+            int segments = program.Factorial(n) / (program.Factorial(h - 1) * program.Factorial(n - h + 1));
+
+            //Простое число simpleNum задаётся с клавиатуры
+            int simpleNum = 0;
+            Console.Write("Введите простое число p: ");
+            bool isNotSimple = true;
+            while (isNotSimple)
+            {
+                simpleNum = Convert.ToInt32(Console.ReadLine());
+                for (int i = 1; i < Math.Ceiling(Math.Sqrt(simpleNum)); i++)
+                {
+                    if (simpleNum % i == 0 && i != 1 && i != simpleNum)
+                    {
+                        Console.Write("Число не является простым! Повторите ввод: ");
+                        break;
+                    }
+                    else if (i == Math.Ceiling(Math.Sqrt(simpleNum)) - 1) isNotSimple = false;
+                }
+            }
+            Console.WriteLine();
+
+            //Вектор ключ имеет формат {a1 ; b2; c3 ; ...}, все элементы меньше заданного простого числа simpleNum
+            List<int> key = new List<int>();
+            Console.WriteLine("Сколько элементов в вектор ключе? ");
+            Console.Write("Элементов = ");
+            int count = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Введите элементы вектор ключа");
+            Console.WriteLine("Каждый элемент ключа > 0, целый и < простого числа p");
+            for (int i = 0; i < count; i++)
+            {
+            input:
+                Console.Write("Элемент {0} = ", i + 1);
+                int buferInput = Convert.ToInt32(Console.ReadLine());
+                if (buferInput > 0 && buferInput < simpleNum) key.Add(buferInput);
+                else
+                {
+                    Console.WriteLine("Недопустимый элемент вектор ключа! Повторите ввод");
+                    goto input;
+                }
+            }
+            Console.WriteLine();
+
+            //Создаём объект класса FragKey и генерируем фрагменты вектр ключа
+            FragKey frag = new FragKey();
+            int[,] segmentsKey = frag.Frag(segments, simpleNum, ref key);
+
+            //Выводим сегменты ключа на консоль
+            Console.WriteLine("Сегменты вектор ключа: ");
+            for (int i = 0; i < segmentsKey.GetLength(0); i++)
+            {
+                for (int j = 0; j < segmentsKey.GetLength(1); j++) Console.Write(segmentsKey[i, j] + " ");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            //Массив карта доступа сотрудников к частям ключа
+            int[,] accessMap = new int[segments, n];
+
+            //Создаём объект класса Majority и заполняем массив-матрицу доступа сотрудников к элем. секрета
+            CustomLimit customLimit = new CustomLimit();
+            customLimit.ShareMethod(segments, n, h, ref accessMap);
+
+            //Выводим матрицу разделения секретных фрагментов ключа между сотрудниками
+            Console.WriteLine("Матрица мажоритарного разделения секрета для {0} сотрудников: ", n);
+            for (int i = 0; i < accessMap.GetLength(0); i++)
+            {
+                for (int j = 0; j < accessMap.GetLength(1); j++)
+                {
+                    Console.Write(accessMap[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+
+        private int Factorial(int num)
+        {
+            int bufer = 1;
+            for (int i = 1; i <= num; i++) bufer *= i;
+            return bufer;
+        }
+    }
+}
